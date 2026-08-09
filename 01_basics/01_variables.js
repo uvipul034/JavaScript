dbd23b731592f684e91993ca42a1641e8cf4c1d6
uvipul034 <uvipul034@gmail.com>
2026-08-09 16:40:48 +0000

    Variables & Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 13254
+let accountEmail = "vipul@google.com"
+var accountPassword = "12345"
+accountCity = "Meerut"
+/*
+Prefer not to use var use let instead;
+*/
+accountCity = "Muzaffarnagar"
+accountEmail ="sangam@google.com"
+accountPassword ="54321"
+// console.log(accountCity);
+console.table([accountCity, accountEmail, accountPassword]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3); we are using node js npot browser so this will show error while being ran 
+
+/* 
+console.log(typeof "Vipul")
+console.log(typeof undefined); => undefined
+console.log(typeof null); => object
+Output will be string it is used to know the datatype of any variable 
+Primitive Datatypes:
+1.number => 2 to the power 53
+2.bigint (used in trading /stock markets or big websites like reddit)
+3.string => ""
+4.boolean => true/false
+5.null => standalone value
+6.undefined => Not assigned value yet
+7.symbol => unique
+*/
