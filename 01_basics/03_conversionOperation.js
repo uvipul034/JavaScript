0cf42041a69709c485891c2a5fbc90e2502af73f
uvipul034 <uvipul034@gmail.com>
2026-08-09 17:13:39 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+/*
+Conversion:
+"33" => 33
+"33abc" => NaN
+true => 1; false => 0
+*/
+  
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0=> false
+"" => false
+"Vipul" => true
+*/
+
+let x = 33
+
+let stringNumber = String(x)
+console.log(stringNumber);
